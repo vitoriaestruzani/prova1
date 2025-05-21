890228d34185c5d428f79b78341dceacade47d1d
vitoriaestruzani <estruzanivitoriaa@gmail.com>
2025-05-21 11:47:33 -0300

    logicaprogramacao

---
+++ backend/backend/backend/logica.programação/01Variaveis.py
@@ -0,0 +1,29 @@
+nome= "vivi"
+idade= 17
+salario= 350
+legal= True
+
+print("nome",nome)
+print("idade",idade)
+print("salario", salario)
+print("é legal", legal)
+
+song= "505"
+year= 2007
+minutes= 4.14
+isitcool= True
+
+print("Song name", song)
+print("Year of release", year)
+print("time", minutes)
+print("is it cool?", isitcool)
+
+animal= "ranccoon"
+weight= 15
+bodylength= 50.7
+dangerous= True
+
+print("animal", animal)
+print("weight", weight)
+print("body length?", bodylength)
+print("is it dangerous?", dangerous)
---
+++ backend/backend/backend/logica.programação/02operacoesMatematicas.py
@@ -0,0 +1,42 @@
+a = 10
+b = 20
+soma = a + b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+
+
+
+print("A soma é:", soma)
+print("a subtracao é:", subtracao)
+print("A multiplicacao é:", multiplicacao)
+print("a divisao é:", divisao)
+
+
+a = 67
+b = 25678
+soma = a + b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+
+
+
+print("A soma é:", soma)
+print("a subtracao é:", subtracao)
+print("A multiplicacao é:", multiplicacao)
+print("a divisao é:", divisao)
+
+a = 50
+b = 30
+soma = a + b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+
+
+
+print("A soma é:", soma)
+print("a subtracao é:", subtracao)
+print("A multiplicacao é:", multiplicacao)
+print("a divisao é:", divisao)
