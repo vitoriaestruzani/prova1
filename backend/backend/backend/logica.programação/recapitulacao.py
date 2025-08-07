a6184cce6470071eb1489b069d43d88671c5e7e1
vitoriaestruzani <estruzanivitoriaa@gmail.com>
2025-08-07 09:16:57 -0300

    7-8

---
+++ backend/backend/backend/logica.programação/recapitulacao.py
@@ -0,0 +1,40 @@
+def caixa_eletronico():
+    nome = input("Digite seu nome para cadastrar a conta: ")
+    saldo = 0.00
+    opcao = ""
+
+    while opcao != "Sair":
+        print("\nMenu:")
+        print("1 - Depositar")
+        print("2 - Sacar")
+        print("3 - Ver Saldo")
+        print("Sair")
+        
+        opcao = input("Escolha uma opção: ")
+
+        match opcao:
+            case "1":
+                valor = float(input("Valor para depositar: R$ "))
+                saldo += valor
+                print("Depósito realizado.")
+
+            case "2":
+                valor = float(input("Valor para sacar: R$ "))
+                if valor <= saldo:
+                    saldo -= valor
+                    print("Saque realizado.")
+                else:
+                    print("Saldo insuficiente.")
+
+            case "3":
+                print("Saldo atual: R$", saldo)
+
+            case "Sair":
+                print("Até logo," + nome + "!")
+
+            case _:
+                print("Opção inválida.")
+
+
+                
+caixa_eletronico()
