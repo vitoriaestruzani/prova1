e0921229a939b6d0c1143eda6af8e146fd5af043
vitoriaestruzani <estruzanivitoriaa@gmail.com>
2025-09-19 11:45:13 -0300

    html,css

--- gerenciador_web/config/tarefas/models.py
+++ gerenciador_web/config/tarefas/models.py
@@ -1,9 +1,10 @@
 
 from django.db import models
+
 class Tarefa(models.Model):
 
-    Titulo = models.CharField(max_length = 200)
-    Descricao = models.TextField(blank = True, null = True)
+    titulo = models.CharField(max_length = 200)
+    descricao = models.TextField(blank = True, null = True)
     data_criacao = models.DateTimeField(auto_now_add = True)
     concluida = models.BooleanField(default = False)
 
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Tarefa
 
-# Create your views here.
+
+def listar_tarefas(request):
+    #1. A busca no banco de dados
+    tarefas_salvas = Tarefa.objects.all()
+
+    #2. Criamos um "dicionário de contexto" para enviar os dados ao tempate.
+    #3. A chave 'minhas_tarefas' será a variável que usaremos no HTML.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas
+    }
+
+    #3. Renderizamos o template, passando a requisição e o contexto com s dados.
+    return render(request, 'tarefas/lista.html', contexto)
