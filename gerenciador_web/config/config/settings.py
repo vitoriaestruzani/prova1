3eea1d21e1d8ecae67f554bb228f21392603f70e
vitoriaestruzani <estruzanivitoriaa@gmail.com>
2025-10-08 11:57:57 -0300

    agr vai

--- gerenciador_web/config/config/settings.py
+++ gerenciador_web/config/config/settings.py
@@ -115,7 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Durante desenvolvimento, adicione isso também:
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "tarefas/static",
